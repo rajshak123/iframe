155feaa09649b982f14b91ffe097792cafa89e66
rs <rs@localhost.localdomain>
2021-08-22 23:10:03 +0530

    rearranging divs

--- src/mainDashboard.js
+++ src/mainDashboard.js
@@ -21,39 +21,45 @@ class Dashboard extends Component {
 
   render() {
       const { username } = this.state;
-      let widht1 = document.getElementsByClassName('view')[0] && getComputedStyle(document.getElementsByClassName('view')[0]).width;
-      let height1 = document.getElementsByClassName('view')[0] && getComputedStyle(document.getElementsByClassName('view')[0]).height;
+      let widht1 = document.getElementsByClassName('view1')[0] && getComputedStyle(document.getElementsByClassName('view1')[0]).width;
+      let height1 = document.getElementsByClassName('view1')[0] && getComputedStyle(document.getElementsByClassName('view1')[0]).height;
 
-      let widht2 = document.getElementsByClassName('view')[1] && getComputedStyle(document.getElementsByClassName('view')[1]).width;
-      let height2 = document.getElementsByClassName('view')[1] && getComputedStyle(document.getElementsByClassName('view')[1]).height;
+      let widht2 = document.getElementsByClassName('view2')[1] && getComputedStyle(document.getElementsByClassName('view2')[1]).width;
+      let height2 = document.getElementsByClassName('view2')[1] && getComputedStyle(document.getElementsByClassName('view2')[1]).height;
 
     return (
-    <div>
-        <div className="navContainer">
-            <div className='userName'>{username}</div>
-            <input className='inputTxtView inputField'></input>
-            <input className='inputTxtView inputField'></input>
-            <button>LogOut</button>
-        </div>
         <div className='viewContainer'>
-            <div className='view'>
-              <iframe 
-                  is="x-frame-bypass" 
-                  src="https://www.youtube.com/"
-                  width={widht1}
-                  height={height1}
-              ></iframe>
+
+            <div className='view1'>
+              <div className='inputContainer'>
+                <div className='userName'>{username}</div>
+                <input className='inputTxtView inputField'></input>
+              </div>
+              <div className='view'> 
+                <iframe 
+                    is="x-frame-bypass" 
+                    src="https://www.youtube.com/"
+                    width={widht1}
+                    height={height1}
+                ></iframe>
+              </div>
+
             </div>
-            <div className='view'>
-              <iframe 
-                  is="x-frame-bypass" 
-                  src="https://www.digitalocean.com/community/tutorials/how-to-handle-routing-in-react-apps-with-react-router"
-                  width={widht2}
-                  height={height2}
-              ></iframe>
+            <div className='view2'>
+              <div className='inputContainer'>
+                <input className='inputTxtView inputField'></input>
+                <button className='loginBtn logOutBtn'>LogOut</button>
+              </div>
+              <div className='view'> 
+                <iframe 
+                    is="x-frame-bypass" 
+                    src="https://www.digitalocean.com/community/tutorials/how-to-handle-routing-in-react-apps-with-react-router"
+                    width={widht1}
+                    height={height1}
+                ></iframe>
+              </div>
             </div>
         </div>
-    </div>
 
     );
   }
